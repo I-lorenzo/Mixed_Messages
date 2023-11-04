b0c5e4734f0165b294733c3a92ee6ef7e6b8efbd
Lorenzo <codelorenzo@outlook.com>
2023-11-04 20:18:36 +0000

    Added coments

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 //Future predictions
+
+// object
 const future = {
     first:'Things are looking great!',
-    second:'There is a change you win the lottery! if you play today',
+    second:'There is a chance you win the lottery! if you play today',
     three:'To day you should stay inside its not wise to try new things', 
-    four:'Things are looking real bright',
+    four:'Things are looking real bright go on a adventure',
     five:'The grass is not allways greener on the other side',
     six:'The best way to predict the future is to create it.',
     seven:'Everything changes with time.',
     eight:'The best way to predict the future is to study the past'
 };
-
+//covert the objects values into an array
 const predictions = Object.values(future);
 
+//function to get random element from array
 function getRandom(array) {
     const randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
 
 }
+
+//loop over the objects using Math.random
 for (let i = 0; i < 1; i++) {
     const randomValue = getRandom(predictions);
     console.log(randomValue);
