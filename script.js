82f44614f99f6bb0a4d44b25c81fe0b158ec456a
Lorenzo <codelorenzo@outlook.com>
2023-11-06 10:13:51 +0000

    object changes

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 
 // object
 const future = {
-    first:'Things are looking great!',
-    second:'There is a chance you win the lottery! if you play today',
-    three:'To day you should stay inside its not wise to try new things', 
-    four:'Things are looking real bright go on a adventure',
-    five:'The grass is not allways greener on the other side',
-    six:'The best way to predict the future is to create it.',
+    one:'Things are looking great!',
+    two:'There is a chance you win the lottery! if you play today!',
+    three:'To day you should not try new things!', 
+    four:'Things are looking real bright go on a adventure!',
+    five:'The grass is not allways greener on the other side!',
+    six:'The best way to predict the future is to create it!',
     seven:'Everything changes with time.',
-    eight:'The best way to predict the future is to study the past'
+    eight:'The best way to predict the future is to study the past!'
 };
-//covert the objects values into an array
+//convert the objects values into an array
 const predictions = Object.values(future);
 
 //function to get random element from array
